1dec413eead29a2873bc6acfe7ea164b8a9dec8b
nikhil608 <61872201+nikhil608@users.noreply.github.com>
2020-03-06 18:37:38 +0530

    first upload

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+// connect database
+connectDB();
+
+app.get('/', (req,res) => res.send('API running'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`server start on port ${PORT}`));
