4c5693eafbb88185e96ced48ce04068dc50dfc30
nikhil123 <manoj@wegile.com>
2020-03-12 19:06:14 +0530

    middle ware

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const User = require('../../model/user');
 
-router.get('/', (req, res) => res.send("auth route"));
+router.get('/', auth, async (req, res) => {
+    try{
+        const user = await User.findById(req.user.id).select('-password');
+        console.log("user", user);
+        res.json(user);
+    } catch(err){
+        console.log("err.message", err.message);        
+        res.json(err.message);
+    }
+});
 
 module.exports = router;
